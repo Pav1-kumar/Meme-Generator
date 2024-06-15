bf9c69a60b993518466052185baab3e9da9d8bfb
Pav1-kumar <pavankumarmadag627@gmail..com>
2024-06-15 12:25:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header.js";
+import Meme from "./Meme.js"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App"> 
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+ 
